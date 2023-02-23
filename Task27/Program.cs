d010880b9075ede1a0ebcf81ea911577eee3afcf
Konstantin Chern <codahl@yandex.ru>
2023-02-24 00:35:59 +0300

    Solved Task27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -5,3 +5,26 @@
 // 82 -> 10
 // 9012 -> 12
 
+
+Console.Clear();
+
+int Inputint(string prompt)
+{
+    System.Console.Write($"{prompt} >");
+    return Convert.ToInt32(Console.ReadLine());
+
+}
+
+int numberSumm(int number)
+{
+    int result = 0;
+    for (int i = 1; number > 0; i++)
+    {
+        result = number % 10 + result;
+        number = number / 10;
+    }
+    return result;
+}
+
+int userNumber = Inputint("Введите число");
+System.Console.WriteLine($"Сумма цифр числа {userNumber} = {numberSumm(userNumber)}");
